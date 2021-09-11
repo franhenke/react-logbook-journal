f1722c6f76caae84af741f2b00ab02aa4abf2bd5
Fran Henke <franci.henke@gmail.com>
2021-09-11 22:11:34 +0200

    Add info window on markers / creation date

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -8,7 +8,8 @@ import {
 } from '@react-google-maps/api'
 import { formatRelative } from 'date-fns'
 import '@reach/combobox/styles.css'
-import { current } from 'immer'
+import { useCallback } from 'react'
+import { useRef } from 'react'
 
 const libraries = ['places']
 const mapContainerStyle = {
@@ -33,7 +34,23 @@ export default function MapComponent() {
     libraries,
   })
   const [markers, setMarkers] = useState([])
-  const markerImage = '/public/bear.svg?raw=true'
+  const [selectedMarker, setSelectedMarker] = useState(null)
+
+  const onMapClick = useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ])
+  }, [])
+
+  const mapRef = useRef()
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+  }, [])
 
   if (loadError) return 'Error loading maps'
   if (!isLoaded) return 'Loading Maps'
@@ -45,29 +62,40 @@ export default function MapComponent() {
         zoom={8}
         center={center}
         options={options}
-        onClick={(event) => {
-          setMarkers((current) => [
-            ...current,
-            {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-              time: new Date(),
-            },
-          ])
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
         {markers.map((marker) => (
           <Marker
             key={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
-              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+              url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
               scaledSize: new window.google.maps.Size(30, 30),
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(15, 15),
             }}
+            onClick={() => {
+              setSelectedMarker(marker)
+            }}
           />
         ))}
+        {selectedMarker ? (
+          <InfoWindow
+            position={{
+              lat: selectedMarker.lat,
+              lng: selectedMarker.lng,
+            }}
+            onCloseClick={() => {
+              setSelectedMarker(null)
+            }}
+          >
+            <div>
+              <h2>Your memory</h2>
+              <p>Created: {formatRelative(selectedMarker.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   )
