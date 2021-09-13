2001b1c4afb014a5efe07d0e07425b74bbf442e0
Fran Henke <franci.henke@gmail.com>
2021-09-13 23:04:22 +0200

    Change design for journal details view

--- src/components/JournalDetails/JournalDetails.js
+++ src/components/JournalDetails/JournalDetails.js
@@ -45,62 +45,60 @@ export const JournalDetailsComponent = () => {
     setEditing(false)
   }
 
-  const {
-    image,
-    place,
-    date,
-    category,
-    caption,
-    entry,
-    bookmarked,
-  } = selectedEntry
+  const { image, place, date, category, caption, entry, bookmarked } =
+    selectedEntry
 
   return (
-    <Frame screenName="Detail">
-      <div className="featured-image-wrapper">
-        {image ? <img src={image} alt="" /> : <img src={heroImage} alt="" />}
-      </div>
-      <div className="content-container">
-        <h3 className="details-place">{place}</h3>
-        <h3 className="details-category">{category}</h3>
-        <h3 className="details-caption">{caption}</h3>
-        <h3 className="details-date">{date}</h3>
-        <h3 className="details-entry">{entry}</h3>
-
-        <div className="button-container">
-          {bookmarked === true ? (
-            <button className="bookmark-icon" onClick={bookmarkHandler}>
-              <img src={bookmarkIconFilled} alt="" />
-            </button>
-          ) : (
-            <button className="bookmark-icon" onClick={bookmarkHandler}>
-              <img src={bookmarkIcon} alt="" />
-            </button>
-          )}
-
+    <Frame screenName="Details">
+      <div className="journal-details">
+        <div className="journal-details__featured-image-wrapper">
+          {image ? <img src={image} alt="" /> : <img src={heroImage} alt="" />}
           <button
-            className="details-button-edit"
+            className="journal-details__button-edit"
             onClick={() => editRow(selectedEntry)}
           >
             <img src={edit} alt="" />
           </button>
         </div>
-      </div>
 
-      <Dialog
-        fullWidth={true}
-        open={editing}
-        onClose={handleClose}
-        aria-labelledby="edit-form-dialog"
-      >
-        <DialogContent>
-          <EditJournalForm
-            setEditing={setEditing}
-            selectedJournal={selectedJournal}
-            updateJournal={updateJournal}
-          />
-        </DialogContent>
-      </Dialog>
+        <div className="journal-details__content-container">
+          <h3 className="journal-details__place">{place}</h3>
+          <h3 className="journal-details__category">{category}</h3>
+          <h3 className="journal-details__caption">{caption}</h3>
+          <h3 className="journal-details__date">{date}</h3>
+          <h3 className="journal-details-entry">{entry}</h3>
+          {bookmarked === true ? (
+            <button
+              className="journal-details__bookmark-icon"
+              onClick={bookmarkHandler}
+            >
+              <img src={bookmarkIconFilled} alt="" />
+            </button>
+          ) : (
+            <button
+              className="journal-details__bookmark-icon"
+              onClick={bookmarkHandler}
+            >
+              <img src={bookmarkIcon} alt="" />
+            </button>
+          )}
+        </div>
+
+        <Dialog
+          fullWidth={true}
+          open={editing}
+          onClose={handleClose}
+          aria-labelledby="edit-form-dialog"
+        >
+          <DialogContent>
+            <EditJournalForm
+              setEditing={setEditing}
+              selectedJournal={selectedJournal}
+              updateJournal={updateJournal}
+            />
+          </DialogContent>
+        </Dialog>
+      </div>
     </Frame>
   )
 }
